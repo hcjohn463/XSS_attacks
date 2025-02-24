d8b7042d6c57d02741c1db9c3ae8f146c0d9e358
林子堯 <84957154+lin891020@users.noreply.github.com>
2025-02-24 11:02:10 +0800

    update reflected xss

--- xss_lab/reflected_xss/index.php
+++ xss_lab/reflected_xss/index.php
@@ -7,10 +7,9 @@
     <h2>搜尋結果：</h2>
     <?php
         if (isset($_GET['q'])) {
-            $query = $_GET['q']; // ❌ 沒有過濾，容易被 XSS 攻擊
+            $query = $_GET['q']; // ❌ 未過濾，會導致 XSS
             echo "<p>你搜尋的是：" . $query . "</p>";
         }
     ?>
 </body>
 </html>
-.
